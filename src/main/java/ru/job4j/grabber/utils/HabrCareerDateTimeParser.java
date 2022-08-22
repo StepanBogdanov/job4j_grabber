9413b30ef0e1b78e93c3f636d808d3006e321f61
Stepan Bogdanov <bogstepan@gmail.com>
2022-08-22 16:39:26 +0300

    2.1. Преобразование даты [#289476 #335087]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -1,13 +1,13 @@
 package ru.job4j.grabber.utils;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        String dateTime = parse.split("\\+")[0];
-        return LocalDateTime.parse(dateTime);
+        return LocalDateTime.from(ZonedDateTime.parse(parse));
     }
 
 }
